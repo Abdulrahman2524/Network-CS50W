deb4c4415730c8b79a820c7d39440b0392b8af44
Abdulrahman2524 <abdulrahmanalkhyat@gmail.com>
2023-04-09 00:09:05 +0200

    Made the first model

--- network/models.py
+++ network/models.py
@@ -4,3 +4,11 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class newPost(models.Model):
+    text = models.TextField(null=True)
+    date = models.DateTimeField(auto_now_add=True, null=True)
+    user = models.ForeignKey(User, related_name="user_post", on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f"{self.user} | {self.text} | {self.date}"
--- network/tests.py
+++ network/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
-
+from .models import *
+from datetime import datetime
 # Create your tests here.
+
+class PostTestCase(TestCase):
+    def setUp(self):
+        # create post.
+        a1 = newPost.objects.create(text="Good Night!", date=datetime.now())
+        a2 = newPost.objects.create(text="Good Bye!", date=datetime.now())
+
+    # test.
+    def test_post(self):
+        a = newPost.objects.all()
+        self.assertEqual(a.count(), 2)
